b6c2a2358cb38076e594c8d8cbe42a695f1540e8
pmsoo <pmsoo4@naver.com>
2025-04-06 04:22:05 +0900

    Initial

--- src/main/java/org/zerock/guestbook/GuestbookApplication.java
+++ src/main/java/org/zerock/guestbook/GuestbookApplication.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+
 public class GuestbookApplication {
 
     public static void main(String[] args) {
